7ea4ceead7b0fcbaa57c5edbe4eecaa86a0623be
ICEcoldShallom <somtoezeoke.develop@gmail.com>
2025-07-21 20:22:41 +0100

    User Input Learning

---
+++ Introduction_To_Python/IntroducingInput.py
@@ -0,0 +1,10 @@
+# The input() keyword provides user input (default type is the string data type)
+print("Enter a number: ")
+num1 = int(input())
+
+print("Enter another number: ")
+num2 = int(input())
+
+sum1 = num1 + num2
+
+print("The sum equals: " + str(sum1))
