9bc0cc33a19245d17eb04461854fc6ae62753010
ICEcoldShallom <somtoezeoke.develop@gmail.com>
2025-07-23 18:11:12 +0100

    Mathematical Functions Learning

---
+++ Introduction_To_Python/MathFunctions.py
@@ -0,0 +1,31 @@
+# This enables the usage of functions for mathematical operations. 
+import math
+
+height = 1.78
+
+# Rounds double/float number to nearest integer
+print(round(height))
+
+# Prints the value of PI
+print(math.pi)
+
+# Rounds the number up
+print(math.ceil(height))
+
+# Rounds the number down
+print(math.floor(height))
+
+# Prints the positive version of the value.
+print(abs(-1*height))
+
+# Raises the base by the power of the exponent.
+print(pow(height,2))
+
+# Prints the square root of the value.
+print(math.sqrt(height))
+
+# Returns and prints the maximum value with given arguments.
+print(max(height,2.00))
+
+# Returns and prints the minimum value with given arguments.
+print(min(height,2.00))
